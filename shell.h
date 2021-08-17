85067b36bee2446ac4e7d3140bf37ee123387144
joshua onwuka <contactuche02@gmail.com>
2021-08-17 11:26:37 -0700

    updated

--- shell.h
+++ shell.h
@@ -12,27 +12,27 @@
 extern char **environ;
 
 /**
- *  * struct environment - struct for a linked list of environment variables
- *   * @key: key name for variable
- *    * @value: value of variable
- *     * @next: pointer to next variable
- *      */
+ * struct environment - struct for a linked list of environment variables
+ * @key: key name for variable
+ * @value: value of variable
+ * @next: pointer to next variable
+ */
 typedef struct environment
 {
-		char *key;
-			char *value;
-				struct environment *next;
+	char *key;
+	char *value;
+	struct environment *next;
 } env_t;
 
 /**
- *  * struct builtins - struct for the builtin functions
- *   * @name: name of builtin command
- *    * @f: function for corresponding builtin
- *     */
+ * struct builtins - struct for the builtin functions
+ * @name: name of builtin command
+ * @f: function for corresponding builtin
+ */
 typedef struct builtins
 {
-		char *name;
-			void (*f)();
+	char *name;
+	void (*f)();
 } builtins_t;
 
 ssize_t _puts(char *str);
@@ -47,5 +47,6 @@ int check_for_path(char *av[], env_t **env);
 void path_execute(char *command, char **args);
 env_t *find_path(env_t *head);
 unsigned int _strlen(char *str);
+
 
 #endif /* _SHELL_H_ */
