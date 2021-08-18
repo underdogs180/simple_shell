f54b5b04cb23270345b68da8720c37b16b2f953d
joshua onwuka <contactuche02@gmail.com>
2021-08-18 04:28:57 -0700

    modified

--- builtins.c
+++ builtins.c
@@ -7,21 +7,52 @@
  */
 void (*check_for_builtins(vars_t *vars))(vars_t *vars)
 {
-  unsigned int i;
-  builtins_t check[] = {
-			{"exit", new_exit},
-			{"env", _env},
-			{"setenv", NULL},
-			{"unsetenv", NULL},
-			{NULL, NULL}
-  };
+	unsigned int i;
+	builtins_t check[] = {
+		{"exit", new_exit},
+		{"env", _env},
+		{"setenv", NULL},
+		{"unsetenv", NULL},
+		{NULL, NULL}
+	};
 
-  for (i = 0; check[i].f != NULL; i++)
-    {
-      if (_strcmpr(vars->av[0], check[i].name) == 0)
-	break;
-    }
-  if (check[i].f != NULL)
-    check[i].f(vars);
-  return (check[i].f);
+	for (i = 0; check[i].f != NULL; i++)
+	{
+		if (_strcmpr(vars->av[0], check[i].name) == 0)
+			break;
+	}
+	if (check[i].f != NULL)
+		check[i].f(vars);
+
+	return (check[i].f);
 }
+
+/**
+ * new_exit - exit program
+ * @vars: variables
+ * Return: void
+ */
+void new_exit(vars_t *vars)
+{
+	free(vars->buffer);
+	free(vars->av);
+	free_env(vars->env);
+	exit(vars->status);
+}
+
+/**
+ * _env - prints the current environment
+ * @vars: struct of variables
+ * Return: void.
+ */
+void _env(vars_t *vars)
+{
+	unsigned int i;
+
+	for (i = 0; vars->env[i]; i++)
+	{
+		_puts(vars->env[i]);
+		_puts("\n");
+	}
+}
+
