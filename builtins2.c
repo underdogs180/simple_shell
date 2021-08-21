d50fd1ec519ede3fe83d53207f482f06acaf00a2
underdogs180 <asadirshad38@gmail.com>
2021-08-21 11:09:43 -0700

    Updated

--- builtins2.c
+++ builtins2.c
@@ -8,29 +8,29 @@
  */
 void add_key(vars_t *vars)
 {
-unsigned int i;
-char **newenv;
+  unsigned int i;
+  char **newenv;
 
-for (i = 0; vars->env[i] != NULL; i++)
-;
-newenv = malloc(sizeof(char *) * (i + 2));
-if (newenv == NULL)
-{
-print_error(vars, NULL);
-vars->status = 127;
-new_exit(vars);
-}
-for (i = 0; vars->env[i] != NULL; i++)
-newenv[i] = vars->env[i];
-newenv[i] = add_value(vars);
-if (newenv[i] == NULL)
-{
-free(newenv);
-exit(127);
-}
-newenv[i + 1] = NULL;
-free(vars->env);
-vars->env = newenv;
+  for (i = 0; vars->env[i] != NULL; i++)
+    ;
+  newenv = malloc(sizeof(char *) * (i + 2));
+  if (newenv == NULL)
+    {
+      print_error(vars, NULL);
+      vars->status = 127;
+      new_exit(vars);
+    }
+  for (i = 0; vars->env[i] != NULL; i++)
+    newenv[i] = vars->env[i];
+  newenv[i] = add_value(vars);
+  if (newenv[i] == NULL)
+    {
+      free(newenv);
+      exit(127);
+    }
+  newenv[i + 1] = NULL;
+  free(vars->env);
+  vars->env = newenv;
 }
 
 /**
@@ -42,18 +42,18 @@ vars->env = newenv;
  */
 char **find_key(char **env, char *key)
 {
-unsigned int i, j, len;
+  unsigned int i, j, len;
 
-len = _strlen(key);
-for (i = 0; env[i] != NULL; i++)
-{
-for (j = 0; j < len; j++)
-if (key[j] != env[i][j])
-break;
-if (j == len && env[i][j] == '=')
-return (&env[i]);
-}
-return (NULL);
+  len = _strlen(key);
+  for (i = 0; env[i] != NULL; i++)
+    {
+      for (j = 0; j < len; j++)
+	if (key[j] != env[i][j])
+	  break;
+      if (j == len && env[i][j] == '=')
+	return (&env[i]);
+    }
+  return (NULL);
 }
 
 /**
@@ -64,27 +64,27 @@ return (NULL);
  */
 char *add_value(vars_t *vars)
 {
-unsigned int len1, len2, i, j;
-char *new;
+  unsigned int len1, len2, i, j;
+  char *new;
 
-len1 = _strlen(vars->av[1]);
-len2 = _strlen(vars->av[2]);
-new = malloc(sizeof(char) * (len1 + len2 + 2));
-if (new == NULL)
-{
-print_error(vars, NULL);
-free(vars->buffer);
-free(vars->av);
-free_env(vars->env);
-return (NULL);
-}
-for (i = 0; vars->av[1][i] != '\0'; i++)
-new[i] = vars->av[1][i];
-new[i] = '=';
-for (j = 0; vars->av[2][j] != '\0'; j++)
-new[i + 1 + j] = vars->av[2][j];
-new[i + 1 + j] = '\0';
-return (new);
+  len1 = _strlen(vars->av[1]);
+  len2 = _strlen(vars->av[2]);
+  new = malloc(sizeof(char) * (len1 + len2 + 2));
+  if (new == NULL)
+    {
+      print_error(vars, NULL);
+      free(vars->buffer);
+      free(vars->av);
+      free_env(vars->env);
+      return (NULL);
+    }
+  for (i = 0; vars->av[1][i] != '\0'; i++)
+    new[i] = vars->av[1][i];
+  new[i] = '=';
+  for (j = 0; vars->av[2][j] != '\0'; j++)
+    new[i + 1 + j] = vars->av[2][j];
+  new[i + 1 + j] = '\0';
+  return (new);
 }
 
 /**
@@ -95,25 +95,25 @@ return (new);
  */
 int _atoi(char *str)
 {
-unsigned int i, digits;
-int num = 0, num_test;
+  unsigned int i, digits;
+  int num = 0, num_test;
 
-num_test = INT_MAX;
-for (digits = 0; num_test != 0; digits++)
-num_test /= 10;
-for (i = 0; str[i] != '\0' && i < digits; i++)
-{
-num *= 10;
-if (str[i] < '0' || str[i] > '9')
-return (-1);
-if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
-return (-1);
-num += str[i] - '0';
-if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
-return (-1);
-}
-if (i > digits)
-return (-1);
-printf("%d\n", num);
-return (num);
+  num_test = INT_MAX;
+  for (digits = 0; num_test != 0; digits++)
+    num_test /= 10;
+  for (i = 0; str[i] != '\0' && i < digits; i++)
+    {
+      num *= 10;
+      if (str[i] < '0' || str[i] > '9')
+	return (-1);
+      if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
+	return (-1);
+      num += str[i] - '0';
+      if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
+	return (-1);
+    }
+  if (i > digits)
+    return (-1);
+  printf("%d\n", num);
+  return (num);
 }
